5b2aad9981699520d88b143b400b1f7e6e9be56f
salindae25 <salindae25@gmail.com>
2021-12-17 13:16:44 +0530

    New UI implemented

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Header from '$lib/header/Header.svelte';
 	import '../app.css';
 </script>
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,23 @@
 <script>
-	import ThemeSelector from '$lib/ThemeSelector.svelte';
-
 	import Todo from '$lib/Todo.svelte';
+
+	let isDark = false;
+
+	function toggle() {
+		isDark = !isDark;
+		document.body.classList.toggle('dark-mode');
+	}
 </script>
 
 <div class="learn-bar">
 	<section class="todoapp">
-		<ThemeSelector />
+		<div class="theme-switch">
+			{#if isDark}
+				<div class="dark-icon" on:click={toggle} />
+			{:else}
+				<div class="light-icon" on:click={toggle} />
+			{/if}
+		</div>
 		<Todo />
 	</section>
-	<!-- <footer class="info">
-		<p>Double-click to edit a todo</p>
-		<p>Created by <a href="http://github.com/petehunt/">salinda</a></p>
-		<p>Inspired by <a href="http://todomvc.com">TodoMVC</a></p>
-	</footer> -->
 </div>
